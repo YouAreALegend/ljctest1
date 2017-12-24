2f03883bec1338eaee449b217ac0e33fa0dbe47f
ljc <2444876939@qq.com>
2017-12-24 21:32:13 +0800

    Modify resortQuestionInstance equal situation.

--- modules/custom/iquiz/src/Form/TakeQuizForm.php
+++ modules/custom/iquiz/src/Form/TakeQuizForm.php
@@ -171,7 +171,7 @@ class TakeQuizForm extends FormBase
     public function resortQuestionInstance(QuestionInstanceInterface $qi1, QuestionInstanceInterface $qi2)
     {
         if ($qi1->get('weight')->value == $qi2->get('weight')->value) {
-            return 0;
+            return 1;
         }
         return $qi1->get('weight')->value > $qi2->get('weight')->value ? 1 : -1;
     }
